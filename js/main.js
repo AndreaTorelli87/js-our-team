857414646136b501b0f72e2c59e346e31b2af478
Andrea Torelli <gioran326@protonmail.com>
2023-03-08 16:23:42 +0100

    Fine + Bonus

--- js/main.js
+++ js/main.js
@@ -1,23 +1,24 @@
 const ourTeam = [
-   {Nome : 'Wayne Barnett', Ruolo : "Founder & CEO",Immagine : "img/wayne-barnett-founder-ceo.jpg"},
-   {Nome : 'Angela Caroll', Ruolo : "Chief Editor", Immagine : "img/angela-caroll-chief-editor.jpg"},
-   {Nome : 'Walter Gordon', Ruolo : "Office Manager",Immagine : "img/walter-gordon-office-manager.jpg"},
-   {Nome : 'Angela Lopez', Ruolo : "Social Media Manager", Immagine : "img/angela-lopez-social-media-manager.jpg"},
-   {Nome : 'Scott Estrada', Ruolo : "Developer", Immagine : "img/scott-estrada-developer.jpg"},
-   {Nome : 'Barbara Ramos', Ruolo : "Graphic Designer",Immagine : "img/barbara-ramos-graphic-designer.jpg"}
+   { Nome: 'Wayne Barnett', Ruolo: "Founder & CEO", Immagine: "img/wayne-barnett-founder-ceo.jpg" },
+   { Nome: 'Angela Caroll', Ruolo: "Chief Editor", Immagine: "img/angela-caroll-chief-editor.jpg" },
+   { Nome: 'Walter Gordon', Ruolo: "Office Manager", Immagine: "img/walter-gordon-office-manager.jpg" },
+   { Nome: 'Angela Lopez', Ruolo: "Social Media Manager", Immagine: "img/angela-lopez-social-media-manager.jpg" },
+   { Nome: 'Scott Estrada', Ruolo: "Developer", Immagine: "img/scott-estrada-developer.jpg" },
+   { Nome: 'Barbara Ramos', Ruolo: "Graphic Designer", Immagine: "img/barbara-ramos-graphic-designer.jpg" }
 ];
 
 let contenitoreDom = document.getElementById("contenitore");
 
 for (var i = 0; i < ourTeam.length; i++) {
    const membroTeam = ourTeam[i];
-   
-   let Nome = `<div>${membroTeam["Nome"]}</div>`;
-   let Ruolo = `<div>${membroTeam["Ruolo"]}</div>`;
-   let Immagine = `<div>
-                     <img src="${membroTeam["Immagine"]}" alt="">
-                  </div>`;
 
-   let membro = Nome + Ruolo + Immagine;
-   contenitoreDom.innerHTML += membro;
+   let test = `<div class="card m-4" style="width: 18rem;">
+                  <img src="${membroTeam["Immagine"]}" class="card-img-top object-fit-contain" alt="image of ${membroTeam["Nome"]}">
+                  <div class="card-body">
+                     <h4 class="card-title">${membroTeam["Nome"]}</h4>
+                     <p class="card-text">${membroTeam["Ruolo"]}</p>
+                  </div>
+               </div>`;
+
+   contenitoreDom.innerHTML += test;
 }
