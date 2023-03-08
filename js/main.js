9596abb67d29de43d0db64ba1843d84022d0aed9
Andrea Torelli <gioran326@protonmail.com>
2023-03-08 15:40:24 +0100

    Bonus 1 OK

--- js/main.js
+++ js/main.js
@@ -1,34 +1,10 @@
 const ourTeam = [
-   {
-      Nome : 'Wayne Barnett',
-      Ruolo : "Founder & CEO",
-      Immagine : "wayne-barnett-founder-ceo.jpg",
-   },
-   {
-      Nome : 'Angela Caroll',
-      Ruolo : "Chief Editor",
-      Immagine : "angela-caroll-chief-editor.jpg",
-   },
-   {
-      Nome : 'Walter Gordon',
-      Ruolo : "Office Manager",
-      Immagine : "walter-gordon-office-manager.jpg",
-   },
-   {
-      Nome : 'Angela Lopez',
-      Ruolo : "Social Media Manager",
-      Immagine : "angela-lopez-social-media-manager.jpg",
-   },
-   {
-      Nome : 'Scott Estrada',
-      Ruolo : "Developer",
-      Immagine : "scott-estrada-developer.jpg",
-   },
-   {
-      Nome : 'Barbara Ramos',
-      Ruolo : "Graphic Designer",
-      Immagine : "barbara-ramos-graphic-designer.jpg",
-   },
+   {Nome : 'Wayne Barnett', Ruolo : "Founder & CEO",Immagine : "img/wayne-barnett-founder-ceo.jpg"},
+   {Nome : 'Angela Caroll', Ruolo : "Chief Editor", Immagine : "img/angela-caroll-chief-editor.jpg"},
+   {Nome : 'Walter Gordon', Ruolo : "Office Manager",Immagine : "img/walter-gordon-office-manager.jpg"},
+   {Nome : 'Angela Lopez', Ruolo : "Social Media Manager", Immagine : "img/angela-lopez-social-media-manager.jpg"},
+   {Nome : 'Scott Estrada', Ruolo : "Developer", Immagine : "img/scott-estrada-developer.jpg"},
+   {Nome : 'Barbara Ramos', Ruolo : "Graphic Designer",Immagine : "img/barbara-ramos-graphic-designer.jpg"}
 ];
 
 let contenitoreDom = document.getElementById("contenitore");
@@ -36,8 +12,12 @@ let contenitoreDom = document.getElementById("contenitore");
 for (var i = 0; i < ourTeam.length; i++) {
    const membroTeam = ourTeam[i];
    
-   for (var chiave in membroTeam) {
-      let aggiungiChiave = `<div>${chiave}: ${membroTeam[chiave]}</div>`;
-      contenitoreDom.innerHTML += aggiungiChiave;
-   }
+   let Nome = `<div>${membroTeam["Nome"]}</div>`;
+   let Ruolo = `<div>${membroTeam["Ruolo"]}</div>`;
+   let Immagine = `<div>
+                     <img src="${membroTeam["Immagine"]}" alt="">
+                  </div>`;
+
+   let membro = Nome + Ruolo + Immagine;
+   contenitoreDom.innerHTML += membro;
 }
